statechannels/apps

commit 553d5146cfbd43cf7643bf21ee76fc301322e775

Remove channelId from isMyTurn

packages/web3torrent/src/clients/payment-channel-client.ts
@@ -220,7 +220,7 @@ export class PaymentChannelClient {
 
     if (!['closing', 'closed'].includes(this.channelCache[channelId].status)) {
       this.channelState(channelId)
-        .pipe(first(cs => this.isMyTurn(cs, channelId)))
+        .pipe(first(cs => this.isMyTurn(cs)))
         .subscribe(cs => {
           logger.info(
             {channelId, cs, me: this.mySigningAddress},
@@ -274,8 +274,7 @@ export class PaymentChannelClient {
     return convertToChannelState(channelResult);
   }
 
-  isMyTurn(state: ChannelState, channelId: string): boolean {
-    // FIXME: Remove channelId param
+  isMyTurn(state: ChannelState): boolean {
     const {payer, beneficiary} = state;
     let myRole: Index;
     if (payer.signingAddress === this.mySigningAddress) myRole = Index.Payer;
@@ -283,7 +282,6 @@ export class PaymentChannelClient {
     else throw 'Not in channel';
 
     return (
-      state.channelId === channelId &&
       state.status === 'running' &&
       state.payer.signingAddress === this.mySigningAddress &&
       state.turnNum
@@ -308,7 +306,7 @@ export class PaymentChannelClient {
   async makePayment(channelId: string, amount: string) {
     let amountWillPay = amount;
     this.channelState(channelId)
-      .pipe(first(cs => this.isMyTurn(cs, channelId)))
+      .pipe(first(cs => this.isMyTurn(cs)))
       .subscribe(async channelState => {
         const {payer, beneficiary} = channelState;
         if (bigNumberify(payer.balance).eq(0)) {

commit cc4b14753973e464252188b188e058a316bf85a2

Define helper functions first
This prevents a runtime error introduced in 553d5146cfbd43cf7643bf21ee76fc301322e775

packages/web3torrent/src/clients/payment-channel-client.ts
@@ -55,6 +55,72 @@ enum Index {
   Beneficiary = 0
 }
 
+const convertToChannelState = (channelResult: ChannelResult): ChannelState => {
+  const {
+    turnNum,
+    channelId,
+    participants,
+    allocations,
+    challengeExpirationTime,
+    status
+  } = channelResult;
+
+  return {
+    channelId,
+    turnNum: utils.bigNumberify(turnNum),
+    status,
+    challengeExpirationTime,
+    beneficiary: {
+      signingAddress: participants[0].participantId,
+      outcomeAddress: participants[0].destination,
+      balance: hexZeroPad(bigNumberify(allocations[0].allocationItems[0].amount).toHexString(), 32)
+    },
+    payer: {
+      signingAddress: participants[1].participantId,
+      outcomeAddress: participants[1].destination,
+      balance: hexZeroPad(bigNumberify(allocations[0].allocationItems[1].amount).toHexString(), 32)
+    }
+  };
+};
+
+const formatParticipants = (
+  aAddress: string,
+  bAddress: string,
+  aOutcomeAddress: string = aAddress,
+  bOutcomeAddress: string = bAddress
+) => [
+  {participantId: aAddress, signingAddress: aAddress, destination: aOutcomeAddress},
+  {participantId: bAddress, signingAddress: bAddress, destination: bOutcomeAddress}
+];
+
+const formatAllocations = (aAddress: string, bAddress: string, aBal: string, bBal: string) => {
+  return [
+    {
+      token: AddressZero,
+      allocationItems: [
+        {destination: aAddress, amount: hexZeroPad(bigNumberify(aBal).toHexString(), 32)},
+        {destination: bAddress, amount: hexZeroPad(bigNumberify(bBal).toHexString(), 32)}
+      ]
+    }
+  ];
+};
+
+const subtract = (a: string, b: string) =>
+  hexZeroPad(
+    bigNumberify(a)
+      .sub(bigNumberify(b))
+      .toHexString(),
+    32
+  );
+
+const add = (a: string, b: string) =>
+  hexZeroPad(
+    bigNumberify(a)
+      .add(bigNumberify(b))
+      .toHexString(),
+    32
+  );
+
 // This class wraps the channel client converting the
 // request/response formats to those used in the app
 
@@ -415,69 +481,3 @@ export class PaymentChannelClient {
 export const paymentChannelClient = new PaymentChannelClient(
   new ChannelClient(window.channelProvider)
 );
-
-const convertToChannelState = (channelResult: ChannelResult): ChannelState => {
-  const {
-    turnNum,
-    channelId,
-    participants,
-    allocations,
-    challengeExpirationTime,
-    status
-  } = channelResult;
-
-  return {
-    channelId,
-    turnNum: utils.bigNumberify(turnNum),
-    status,
-    challengeExpirationTime,
-    beneficiary: {
-      signingAddress: participants[0].participantId,
-      outcomeAddress: participants[0].destination,
-      balance: hexZeroPad(bigNumberify(allocations[0].allocationItems[0].amount).toHexString(), 32)
-    },
-    payer: {
-      signingAddress: participants[1].participantId,
-      outcomeAddress: participants[1].destination,
-      balance: hexZeroPad(bigNumberify(allocations[0].allocationItems[1].amount).toHexString(), 32)
-    }
-  };
-};
-
-const formatParticipants = (
-  aAddress: string,
-  bAddress: string,
-  aOutcomeAddress: string = aAddress,
-  bOutcomeAddress: string = bAddress
-) => [
-  {participantId: aAddress, signingAddress: aAddress, destination: aOutcomeAddress},
-  {participantId: bAddress, signingAddress: bAddress, destination: bOutcomeAddress}
-];
-
-const formatAllocations = (aAddress: string, bAddress: string, aBal: string, bBal: string) => {
-  return [
-    {
-      token: AddressZero,
-      allocationItems: [
-        {destination: aAddress, amount: hexZeroPad(bigNumberify(aBal).toHexString(), 32)},
-        {destination: bAddress, amount: hexZeroPad(bigNumberify(bBal).toHexString(), 32)}
-      ]
-    }
-  ];
-};
-
-const subtract = (a: string, b: string) =>
-  hexZeroPad(
-    bigNumberify(a)
-      .sub(bigNumberify(b))
-      .toHexString(),
-    32
-  );
-
-const add = (a: string, b: string) =>
-  hexZeroPad(
-    bigNumberify(a)
-      .add(bigNumberify(b))
-      .toHexString(),
-    32
-  );