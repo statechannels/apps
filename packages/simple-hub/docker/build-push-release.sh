ab6ac9bab54622bce72af4be88fe175f9bd1aead
Alex Gap <me@alexgap.ca>
2023-02-27 13:46:55 -0800

    get simple-hub deploying

--- packages/simple-hub/docker/build-push-release.sh
+++ packages/simple-hub/docker/build-push-release.sh
@@ -2,6 +2,6 @@
 set -euf -o pipefail
 BASEDIR=$(dirname "$0")
 
-docker build -t registry.heroku.com/simple-hub-production/simple-hub -f "$BASEDIR"/simple-hub.dockerfile "$BASEDIR"/../../..
-docker push registry.heroku.com/simple-hub-production/simple-hub
-heroku container:release -a simple-hub-production simple-hub
+docker build -t registry.heroku.com/simple-hub-hyperspace/simple-hub -f "$BASEDIR"/simple-hub.dockerfile "$BASEDIR"/../../..
+docker push registry.heroku.com/simple-hub-hyperspace/simple-hub
+heroku container:release -a simple-hub-hyperspace simple-hub
